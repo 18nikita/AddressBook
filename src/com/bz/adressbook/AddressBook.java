96844aee9d05e9df1049b5d12cfaaa67c1c2c2b9
unknown <rajputnikita5666@gmail.com>
2021-09-29 15:39:01 +0530

    Added:Welcome message

---
+++ src/com/bz/adressbook/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bz.adressbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// Welcome message
+		System.out.println("....WELCOME TO ADDRESS BOOK....");
+
+	}
+
+}
